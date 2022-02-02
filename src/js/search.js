a5d81d3bf59ead71bf40124b961a66e9f8ebd83d
WebStudioFlip <73443296+WebStudioFlip@users.noreply.github.com>
2022-02-02 10:18:12 +0200

    add

--- src/js/search.js
+++ src/js/search.js
@@ -4,13 +4,13 @@
 import { TheMoviebdhAPI } from './themoviedbAPI';
 import galleryCardsTemplate from '../templates/gallery-elements.hbs';
 
-const searchFormEl = document.querySelector('.js-search-form');
-const galleryListEl = document.querySelector('.js-gallery');
+const searchFormEl = document.querySelector('.search-form');
+const galleryListEl = document.querySelector('.gallery');
 
 const theMoviebdhAPI = new TheMoviebdhAPI();
 
 theMoviebdhAPI.getFavoriteFilms().then(({results:data}) => {
-    console.log(data);
+    console.log(galleryCardsTemplate(data));
     galleryListEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(data));
   });
 
