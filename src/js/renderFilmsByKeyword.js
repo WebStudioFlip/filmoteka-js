7d5e4c5d30fd93a39c77133af7f5a3691ac42b1b
ololosh-tralevich <karpeko76@gmail.com>
2022-02-03 15:27:56 +0200

    upd

--- src/js/renderFilmsByKeyword.js
+++ src/js/renderFilmsByKeyword.js
@@ -6,6 +6,7 @@ import galleryCardsTemplate from '../templates/home-gallery-elements.hbs';
 const searchFormEl = document.querySelector('.search-form');
 const searchField = document.querySelector('.input-box');
 const galleryListEl = document.querySelector('.gallery');
+const headerCont = document.querySelector('div.header-container');
 
 searchFormEl.addEventListener('submit', event => {
   event.preventDefault();
@@ -19,6 +20,10 @@ searchFormEl.addEventListener('submit', event => {
   theMoviebdhAPI.searchFilms(keyword).then(data => {
     if (!data.length) {
       console.log('Не найдено');
+      headerCont.insertAdjacentHTML(
+        'beforeend',
+        '<p class="search-error-text container">Search result not successful. Enter the correct movie name and</p>',
+      );
       return;
     }
     galleryListEl.innerHTML = '';
