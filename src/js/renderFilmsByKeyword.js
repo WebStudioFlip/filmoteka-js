e0eee9052313321066739a08bcb868a2374f5536
ololosh-tralevich <karpeko76@gmail.com>
2022-02-03 14:41:49 +0200

    upd

--- src/js/renderFilmsByKeyword.js
+++ src/js/renderFilmsByKeyword.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 import { TheMoviebdhAPI } from './themoviedbAPI';
 import galleryCardsTemplate from '../templates/home-gallery-elements.hbs';
 
@@ -8,14 +7,21 @@ const searchFormEl = document.querySelector('.search-form');
 const searchField = document.querySelector('.input-box');
 const galleryListEl = document.querySelector('.gallery');
 
-searchFormEl.addEventListener("submit", event => {
-    event.preventDefault();
-    const keyword = searchField.value;
+searchFormEl.addEventListener('submit', event => {
+  event.preventDefault();
+  const keyword = searchField.value;
 
-    galleryListEl.innerHTML = "";
-    const theMoviebdhAPI = new TheMoviebdhAPI();
-    theMoviebdhAPI.searchFilms(keyword).then((data)=>galleryListEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(data)))
-})
+  if (!keyword.length) {
+    return;
+  }
 
-
-
+  const theMoviebdhAPI = new TheMoviebdhAPI();
+  theMoviebdhAPI.searchFilms(keyword).then(data => {
+    if (!data.length) {
+      console.log('Не найдено');
+      return;
+    }
+    galleryListEl.innerHTML = '';
+    galleryListEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(data));
+  });
+});
