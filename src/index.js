cfb68481ac8ee5d7b0a7cf7bf57ca72107466b0f
ololosh-tralevich <karpeko76@gmail.com>
2022-02-04 13:37:08 +0200

    add scroll up btn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './sass/main.scss';
 // import './js/pagination';
 import './js/search';
+// Импорт js кнопки скролла
+import './js/scrollUpBtn';
 // import './js/renderFilmsByKeyword';
 import './js/modal';
