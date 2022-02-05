6f19bc79595f07941e5802a2cde1640143d54c9d
VeronikaTan <90012301+VeronikaTan@users.noreply.github.com>
2022-02-05 15:31:18 -0800

    created function for buttons in my library

--- src/js/myLibraryActive.js
+++ src/js/myLibraryActive.js
@@ -1,6 +1,17 @@
 
-const myLibraryEl = document.querySelector('.library-link');
+const myLibraryWatchedEl = document.querySelector('.watched-button');
+const myLibraryQueueEl = document.querySelector('.queue-button')
 
+myLibraryWatchedEl.addEventListener('click', () => {
 
-  console.log(myLibraryEl);
+        myLibraryWatchedEl.classList.add('is-active')
+        myLibraryQueueEl.classList.remove('is-active')
+    
+})
+myLibraryQueueEl.addEventListener('click', () => {
+    myLibraryWatchedEl.classList.remove('is-active')
+    myLibraryQueueEl.classList.add('is-active')
+})
+ console.dir(myLibraryWatchedEl);
+ 
 
